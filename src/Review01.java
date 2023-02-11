fa95164c9771ac8f77ba9eae1abce6b9c9ce3dba
l53324 <l53324@N9046048.dentsutec.dentsutec.co.jp>
2023-02-11 15:34:23 +0900

    修正版です。

--- src/Review01.java
+++ src/Review01.java
@@ -5,19 +5,17 @@ public class Review01 {
         // TODO 自動生成されたメソッド・スタブ
 
         int num1 = 1500;
-        int num2 = 10;
+        double num2 = 0.1;
         int result;
         result = tax(num1,num2);
         
-        System.out.println(num1 + "円の商品の税込価格は");
-        System.out.println(num1 + result +"円(消費税は");
-        System.out.println(result + "円）です。");     
-                       
+        System.out.println(num1 + "円の商品の税込価格は"+(num1 + result) +"円(消費税は"+result + "円）です。");
+
     }
 
-    private static int tax(int num1, int num2) {
+    private static int tax(int num1, double num2) {
         // TODO 自動生成されたメソッド・スタブ
-        int result = num1 / num2;
+        int result = (int) (num1 * num2);
         return result;
     }
 
